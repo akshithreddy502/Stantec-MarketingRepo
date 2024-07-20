aebb5fe5c46d3564c61f9f2fe90a9a6e9abb7958
akshithreddy <akshithreddythummala@gmail.com>
2024-07-20 15:37:24 -0400

    Initial commit with basic features implemented

---
+++ MarketingCodingAssignment/MarketingCodingAssignment/Services/SearchEngine.cs
@@ -0,0 +1,197 @@
+﻿using Lucene.Net.Analysis;
+using Lucene.Net.Analysis.Standard;
+using Lucene.Net.Documents;
+using Lucene.Net.Index;
+using Lucene.Net.Queries;
+using Lucene.Net.Search;
+using Lucene.Net.Store;
+using Lucene.Net.Util;
+using MarketingCodingAssignment.Models;
+using System.Globalization;
+using CsvHelper;
+using CsvHelper.Configuration;
+using Lucene.Net.Analysis.En;
+
+namespace MarketingCodingAssignment.Services
+{
+    public class SearchEngine
+    {
+        // The code below is roughly based on sample code from: https://lucenenet.apache.org/
+
+        private const LuceneVersion AppLuceneVersion = LuceneVersion.LUCENE_48;
+
+        public SearchEngine()
+        {
+
+        }
+
+        public List<FilmCsvRecord> ReadFilmsFromCsv()
+        {
+            List<FilmCsvRecord> records = new();
+            string filePath = $"{System.IO.Directory.GetCurrentDirectory()}{@"\wwwroot\csv"}" + "\\" + "FilmsInfo.csv";
+            using (StreamReader reader = new(filePath))
+            using (CsvReader csv = new(reader, new CsvConfiguration(CultureInfo.InvariantCulture)))
+            {
+                records = csv.GetRecords<FilmCsvRecord>().ToList();
+
+            }
+            using (StreamReader r = new(filePath))
+            {
+                string csvFileText = r.ReadToEnd();
+            }
+            return records;
+        }
+
+        // Read the data from the csv and feed it into the lucene index
+        public void PopulateIndexFromCsv()
+        {
+            // Get the list of films from the csv file
+            var csvFilms = ReadFilmsFromCsv();
+
+            // Convert to Lucene format
+            List<FilmLuceneRecord> luceneFilms = csvFilms.Select(x => new FilmLuceneRecord
+            {
+                Id = x.Id,
+                Title = x.Title,
+                Overview = x.Overview,
+                Runtime = int.TryParse(x.Runtime, out int parsedRuntime) ? parsedRuntime : 0,
+                Tagline = x.Tagline,
+                Revenue = long.TryParse(x.Revenue, out long parsedRevenue) ? parsedRevenue : 0,
+                VoteAverage = double.TryParse(x.VoteAverage, out double parsedVoteAverage) ? parsedVoteAverage : 0
+            }).ToList();
+
+            // Write the records to the lucene index
+            PopulateIndex(luceneFilms);
+
+            return;
+        }
+
+        public void PopulateIndex(List<FilmLuceneRecord> films)
+        {
+            // Construct a machine-independent path for the index
+            string basePath = Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData);
+            string indexPath = Path.Combine(basePath, "index");
+            using FSDirectory dir = FSDirectory.Open(indexPath);
+
+            // Create an analyzer to process the text
+            StandardAnalyzer analyzer = new(AppLuceneVersion);
+
+            // Create an index writer
+            IndexWriterConfig indexConfig = new(AppLuceneVersion, analyzer);
+            using IndexWriter writer = new(dir, indexConfig);
+
+            //Add to the index
+            foreach (var film in films)
+            {
+                Document doc = new()
+                {
+                    new StringField("Id", film.Id, Field.Store.YES),
+                    new TextField("Title", film.Title, Field.Store.YES),
+                    new TextField("Overview", film.Overview, Field.Store.YES),
+                    new Int32Field("Runtime", film.Runtime, Field.Store.YES),
+                    new TextField("Tagline", film.Tagline, Field.Store.YES),
+                    new Int64Field("Revenue", film.Revenue ?? 0, Field.Store.YES),
+                    new DoubleField("VoteAverage", film.VoteAverage ?? 0.0, Field.Store.YES),
+                    new TextField("CombinedText", film.Title + " " + film.Tagline + " " + film.Overview, Field.Store.NO)
+                };
+                writer.AddDocument(doc);
+            }
+
+            writer.Flush(triggerMerge: false, applyAllDeletes: false);
+            writer.Commit();
+
+           return;
+        }
+
+        public void DeleteIndex()
+        {
+            // Delete everything from the index
+            string basePath = Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData);
+            string indexPath = Path.Combine(basePath, "index");
+            using FSDirectory dir = FSDirectory.Open(indexPath);
+            StandardAnalyzer analyzer = new(AppLuceneVersion);
+            IndexWriterConfig indexConfig = new(AppLuceneVersion, analyzer);
+            using IndexWriter writer = new(dir, indexConfig);
+            writer.DeleteAll();
+            writer.Commit();
+            return;
+        }
+
+        public SearchResultsViewModel Search(string searchString, int startPage, int rowsPerPage, int? durationMinimum, int? durationMaximum, double? voteAverageMinimum)
+        {
+            // Construct a machine-independent path for the index
+            string basePath = Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData);
+            string indexPath = Path.Combine(basePath, "index");
+            using FSDirectory dir = FSDirectory.Open(indexPath);
+            using DirectoryReader reader = DirectoryReader.Open(dir);
+            IndexSearcher searcher = new(reader);
+
+            int hitsLimit = 1000;
+            TopScoreDocCollector collector = TopScoreDocCollector.Create(hitsLimit, true);
+
+            var query = this.GetLuceneQuery(searchString, durationMinimum, durationMaximum, voteAverageMinimum);
+
+            searcher.Search(query, collector);
+
+            int startIndex = (startPage) * rowsPerPage;
+            TopDocs hits = collector.GetTopDocs(startIndex, rowsPerPage);
+            ScoreDoc[] scoreDocs = hits.ScoreDocs;
+
+            List<FilmLuceneRecord> films = new();
+            foreach (ScoreDoc? hit in scoreDocs)
+            {
+                Document foundDoc = searcher.Doc(hit.Doc);
+                FilmLuceneRecord film = new()
+                {
+                    Id = foundDoc.Get("Id").ToString(),
+                    Title = foundDoc.Get("Title").ToString(),
+                    Overview = foundDoc.Get("Overview").ToString(),
+                    Runtime = int.TryParse(foundDoc.Get("Runtime"), out int parsedRuntime) ? parsedRuntime : 0,
+                    Tagline = foundDoc.Get("Tagline").ToString(),
+                    Revenue = long.TryParse(foundDoc.Get("Revenue"), out long parsedRevenue) ? parsedRevenue : 0,
+                    VoteAverage =  double.TryParse(foundDoc.Get("VoteAverage"), out double parsedVoteAverage) ? parsedVoteAverage : 0.0,
+                    Score = hit.Score
+                };
+                films.Add(film);
+            }
+
+            SearchResultsViewModel searchResults = new()
+            {
+                RecordsCount = hits.TotalHits,
+                Films = films.ToList()
+            };
+
+            return searchResults;
+        }
+        private Query GetLuceneQuery(string searchString, int? durationMinimum, int? durationMaximum, double? voteAverageMinimum)
+        {
+            if (string.IsNullOrWhiteSpace(searchString))
+            {
+                // If there's no search string, just return everything.
+                return new MatchAllDocsQuery();
+            }
+
+            var pq = new MultiPhraseQuery();
+            foreach (var word in searchString.Split(" ").Where(s => !string.IsNullOrWhiteSpace(s)))
+            {
+                if (!EnglishAnalyzer.DefaultStopSet.Contains(word))
+                {
+                    pq.Add(new Term("CombinedText", word.ToLowerInvariant()));
+                }
+            }
+
+            Query rq = NumericRangeQuery.NewInt32Range("Runtime", durationMinimum, durationMaximum, true, true);
+            Query vaq = NumericRangeQuery.NewDoubleRange("VoteAverage", 0.0, 10.0, true, true);
+
+            // Apply the filters.
+            BooleanQuery bq = new()
+            {
+                { pq, Occur.MUST },
+                { rq, Occur.MUST }
+            };
+
+            return bq;
+        }
+    }
+}
+
---
+++ README.md
@@ -0,0 +1,35 @@
+# Marketing Coding Test #
+## The Challenge: Modify a Web-based Search App ##
+
+This .NET Core demo app uses [Lucene.NET](https://lucenenet.apache.org/) to index data from the included FilmsInfo.csv file into a search engine. When you run the app, press the reload button to load the data into the search engine. After that, you can enter search terms and press the search button to search for matching movie results. 
+
+This coding test should take 2 hours. For this test, add as many of the basic features as possible in the time allotted. If you have extra time, select one of the advanced features to implement. If you wish to include extra JavaScript libraries, feel free to use a CDN.
+
+## Basic Features: :seedling: ##
+1. Display **Voting Average** in the returned search results.
+2. Finsh conecting the **Voting Average (Minimum)** so that it filters the results that are below the minimum selected values. 
+1. Add **Release Date** to the index and display it in the returned search results. _(:bulb: Hint: You will need to reload the index after making changes to the indexing code.)_
+1. Add a way to filter the search by date range for **Release Date**.
+1. Show off your css skills - improve the styling and layout of the page and/or search results. 
+
+## Advanced Features: :mortar_board: ##
+1. Autocomplete  - suggest search terms as the user types in the search box.
+1. Stemming - when searching for “engineer”, the search should also return results for “engineering”, “engineers”, and “engineered”.
+1. Spell checking - present corrected search terms for user misspellings.
+
+## We’re looking for the following: :eyeglasses: ##
+
+- Can you research and learn something new that you might not be entirely familiar with?
+- Can you manage your time effectively to deliver something of value? 
+- Can you design something that is easy-to-use and visually pleasing?
+- Can you use version control correctly?
+- Can you follow instructions and meet scope requirements?
+
+:warning: When you are finished, please update your readme with a description of which features you implemented and any feedback on the test. Then submit the GitHub link to your finished project to us via email. 
+
+## Updates: ##
+- 2023-06-08 Added some sample code to serve as a starting point (delete indexed data, index new data, perform simple search on indexed data).
+- 2023-06-19 Added a note to update the readme with their own notes and experience.
+- 2023-09-08 Added notes that advanced features are optional due to reduced time expectations. Added notes about what we're looking for.
+- 2023-09-25 Updates to reduce the scope of the assignment and further shorten time requirements.
+- 2024-07-09 Added notes to clarify that assignment github link should be submitted via email.
